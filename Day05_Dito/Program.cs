3e8c63b69772565475029b95f4db7f80be7a8e22
Dito Arjwasa <dito.arjwasa@gmail.com>
2023-12-26 16:41:35 +0700

    added day5 materials

--- Day05_Dito/Program.cs
+++ Day05_Dito/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+	public static void Main(string[] args)
+	{
+		Car car = new Car();
+		Car car1= new Car();
+		Car car2= new Car();
+		
+		Console.WriteLine(Car.count);
+	}
+
+	
+}
+
+abstract class Child
+{
+	protected int value;
+	protected string name;
+	public Child(int value, string name)
+	{
+		this.value = value;
+		this.name = name;
+	}
+	public abstract string CheckHobby();
+	
+}
+
+class Sk8rBoi : Child
+{
+	public Sk8rBoi(int value, string name) : base(value, name){}
+	
+	public override string CheckHobby()
+	{
+		return "Shreddin n Grindin";
+	}
+	
+	
+	
+}
+
+class BalletGirl : Child
+{
+	public BalletGirl(int value, string name) : base(value, name){}
+	public override string CheckHobby()
+	{
+		return "Black Swan starring Natalie Portman";
+	}
+}
+
+class HobbyChecker
+{
+	public void DisplayHobby(Child child)
+	{
+		Console.WriteLine(child.CheckHobby());
+	}
+}
+
+
+public interface IRizz
+{
+	void Rizz();
+}
+
+public interface IGyatt
+{
+	void Gyatt();
+}
+
+public interface IFanumTax
+{
+	void FanumTax();
+}
+
+public interface ISkibidi : IRizz, IFanumTax, IGyatt {}
+public class Rizzler : IRizz
+{
+	public void Rizz ()
+	{
+		Console.WriteLine("Rizz");
+	}	
+}
+
+class Car
+{
+	public static int count = 0;
+	public Car()
+	{
+		Console.WriteLine("Vroom");
+		count++;
+	}
+}
